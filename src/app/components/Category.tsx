3f9442f02dd7d28c3f021cdbfdb57d28734c1b2b
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-10-24 14:59:57 -0300

    Adapt "categories" section to bigger screens

--- src/app/components/Category.tsx
+++ src/app/components/Category.tsx
@@ -8,14 +8,12 @@ type CategoryProps = {
 
 export function Category({ imageSrc, imageAlt, categoryName }: CategoryProps) {
 	return (
-		<div className="flex flex-col gap-5">
-			<figure className="flex flex-col items-center gap-4">
-				<Image src={imageSrc} alt={imageAlt} />
+		<figure className="flex flex-col items-center gap-4 sm:gap-[30px]">
+			<Image src={imageSrc} alt={imageAlt} />
 
-				<figcaption className="font-semibold text-black text-lg">
-					{categoryName}
-				</figcaption>
-			</figure>
-		</div>
+			<figcaption className="font-semibold text-black text-lg sm:text-xl">
+				{categoryName}
+			</figcaption>
+		</figure>
 	);
 }
