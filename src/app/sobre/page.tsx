e6edcfcfcabeafbc34f96c84763cf4ad6e9eae71
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-10-25 13:28:50 -0300

    Adapt about page to bigger screens

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import Image from "next/image";
 
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 import profile from "../img/profile.png";
-import { Footer } from "../components/Footer";
 
 export const metadata: Metadata = {
 	title: "Sobre - Naturella",
@@ -16,34 +16,38 @@ export default function About() {
 		<>
 			<Header />
 
-			<main className="flex flex-col items-center justify-center gap-6 p-6">
-				<Image
-					src={profile}
-					alt="Imagem de uma mulher negra de cabelos cacheados, com uma camisa branca e que está de braços cruzados"
-				/>
+			<main className="mx-auto max-w-[1440px] p-6 lg:p-12">
+				<div className="mx-auto flex max-w-[840px] flex-col items-center justify-center gap-6 lg:flex-row lg:items-start lg:gap-8">
+					<Image
+						src={profile}
+						alt="Imagem de uma mulher negra de cabelos cacheados, com uma camisa branca e que está de braços cruzados"
+						priority
+						width={350}
+					/>
 
-				<div className="flex flex-col gap-5">
-					<p className="font-medium text-base-sm text-black">
-						Bem-vindo ao nosso espaço dedicado à arte e funcionalidade do
-						concreto! Nosso trabalho é transformar um dos materiais mais
-						duráveis e versáteis do mundo em peças únicas que aliam design,
-						resistência e praticidade.
-					</p>
+					<div className="flex flex-col gap-5">
+						<p className="font-medium text-base-sm text-black lg:text-lg">
+							Bem-vindo ao nosso espaço dedicado à arte e funcionalidade do
+							concreto! Nosso trabalho é transformar um dos materiais mais
+							duráveis e versáteis do mundo em peças únicas que aliam design,
+							resistência e praticidade.
+						</p>
 
-					<p className="font-medium text-base-sm text-black">
-						Cada peça é cuidadosamente elaborada, desde o planejamento até a
-						finalização, garantindo um alto padrão de qualidade para quem
-						valoriza tanto a estética quanto a durabilidade. Oferecemos desde
-						itens decorativos até peças funcionais, perfeitas para projetos de
-						arquitetura, design de interiores ou paisagismo.
-					</p>
+						<p className="font-medium text-base-sm text-black lg:text-lg">
+							Cada peça é cuidadosamente elaborada, desde o planejamento até a
+							finalização, garantindo um alto padrão de qualidade para quem
+							valoriza tanto a estética quanto a durabilidade. Oferecemos desde
+							itens decorativos até peças funcionais, perfeitas para projetos de
+							arquitetura, design de interiores ou paisagismo.
+						</p>
 
-					<p className="font-medium text-base-sm text-black">
-						Nosso compromisso é entregar soluções criativas e robustas, ajudando
-						você a trazer mais estilo e modernidade aos seus ambientes. Explore
-						nossas coleções e descubra como o concreto pode transformar qualquer
-						espaço com elegância e resistência.
-					</p>
+						<p className="font-medium text-base-sm text-black lg:text-lg">
+							Nosso compromisso é entregar soluções criativas e robustas,
+							ajudando você a trazer mais estilo e modernidade aos seus
+							ambientes. Explore nossas coleções e descubra como o concreto pode
+							transformar qualquer espaço com elegância e resistência.
+						</p>
+					</div>
 				</div>
 			</main>
 
