79eed2f05c88b1bb28ada78001e26ee0759a95a4
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2025-01-27 20:21:44 -0300

    Add URL prop to Product component and wrap in Link for external navigation

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,10 +1,12 @@
 import Image, { type StaticImageData } from "next/image";
+import Link from "next/link";
 
 type ProductProps = {
 	imageSrc: StaticImageData;
 	imageAlt: string;
 	productName: string;
 	description: string;
+	url: string;
 };
 
 export function Product({
@@ -12,24 +14,31 @@ export function Product({
 	imageAlt,
 	productName,
 	description,
+	url,
 }: ProductProps) {
 	return (
-		<article className="flex h-full max-w-[345px] flex-col sm:max-w-[285px]">
-			<Image
-				src={imageSrc}
-				alt={imageAlt}
-				className="aspect-square object-cover object-center"
-			/>
+		<Link
+			href={url}
+			target="_blank"
+			className="inline-block h-full transition-all hover:scale-105 active:scale-95"
+		>
+			<article className="flex h-full max-w-[345px] flex-col sm:max-w-[285px]">
+				<Image
+					src={imageSrc}
+					alt={imageAlt}
+					className="aspect-square object-cover object-center"
+				/>
 
-			<div className="flex h-full flex-col items-start justify-start gap-2 bg-gray-gray1 p-4">
-				<h3 className="font-semibold text-gray-gray3 text-lg sm:text-xl">
-					{productName}
-				</h3>
+				<div className="flex h-full flex-col items-start justify-start gap-2 bg-gray-gray1 p-4">
+					<h3 className="font-semibold text-gray-gray3 text-lg sm:text-xl">
+						{productName}
+					</h3>
 
-				<span className="font-medium text-gray-gray4 text-sm sm:text-base">
-					{description}
-				</span>
-			</div>
-		</article>
+					<span className="font-medium text-gray-gray4 text-sm sm:text-base">
+						{description}
+					</span>
+				</div>
+			</article>
+		</Link>
 	);
 }
